4dcf0a8d826f4bff0e935fe53eb5d972d925eb80
emak <emilien.ak@gmail.com>
2014-11-21 12:52:44 +0100

    Fix carousel
    ordre d'appel des fichiers js
    
      * Ajout des titres de pages

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,16 @@
-//= require_tree .
+//= require "_jquery"
+//= require "_bootstrap"
+//= require "_carousel"
+
+// Piwik
+var _paq = _paq || [];
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="//metrics.innoveduc.fr/";
+  _paq.push(['setTrackerUrl', u+'piwik.php']);
+  _paq.push(['setSiteId', 2]);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+})();
+
